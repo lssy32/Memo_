c5e8ccda6617a16364da622e0fb9c920966ae918
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-23 19:19:26 +0900

    no message

--- src/Presentation/MemoModifyUI.java
+++ src/Presentation/MemoModifyUI.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class MemoModifyUI implements UI{
 
+
     List<String> list = new ArrayList<String>();
     List<String> list2 = new ArrayList<String>();
     List<String> list3 = new ArrayList<String>();
@@ -17,31 +18,22 @@ public class MemoModifyUI implements UI{
     //임의의 해쉬맵 작성
 
     public void example(){
-        list.add("name");
-        list.add("password");
-        list.add("text");
+        list.add("name1");
 
-        list2.add("name");
-        list2.add("password");
-        list2.add("text");
+        list2.add("name2");
 
-        list3.add("name");
-        list3.add("password");
-        list3.add("text");
+        list3.add("name3");
 
 
         map.put("1",list);
         map.put("2",list2);
         map.put("3",list3);
 
-    }
-
-    public void showExample(){
         for(String i :map.keySet()) {
             System.out.println(i + " : " + map.get(i));
         }
-    }
 
+    }
 
 
 
@@ -49,24 +41,56 @@ public class MemoModifyUI implements UI{
     //첫 화면 UI
     @Override
     public void memoUI() {
+
         Scanner sc = new Scanner(System.in);
+        MemoModifyUI re = new MemoModifyUI();
         System.out.println("===================(1) 메모 목록 조회 =====================");
         System.out.println("=================(2) 수정 할 글번호 입력 ===================");
-        System.out.println("==============(3) 수정 할 글 비밀번호 입력 ==================");
         System.out.println("============== 메뉴의 숫자를 입력하세요. ==================");
         String str1 = sc.next();
+
+        for(int i=0; i<5;i++){
+            if(str1.equals("1")){
+                System.out.println("메뉴목록을 조회합니다.");
+                re.example();
+            }
+            if(str1.equals("2")){
+                System.out.println("수정할 글 번호를 입력하세요.");
+            }
+        }
+
+
 
         switch (str1){
             case "1":
                 System.out.println("메뉴목록을 조회합니다.");
-                showExample();
+                re.example();
+                break;
+            case "2":
+                System.out.println("수정 할 글번호를 입력합니다.");
+                break;
+            case "3":
+                System.out.println("수정할 글 번호를 입력합니다.");
+                break;
 
         }
 
 
 
 
+
+
+
     }
+
+
+    public static void main(String[] args){
+
+        MemoModifyUI re = new MemoModifyUI();
+        re.memoUI();
+
+    }
+
 
     //글 번호 입력
 
