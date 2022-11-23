66ad6ba4bba6013e42d8ffc38f97ccec9f53736b
seung.yeop <milnil0228@gmail.com>
2022-11-23 15:02:12 +0900

    Delete-modified-1

--- src/Data/MemoDelete.java
+++ src/Data/MemoDelete.java
@@ -3,22 +3,22 @@ package Data;
 import java.util.Scanner;
 
 public class MemoDelete {
-    public enterIndex() {
-        System.out.println("============글 번호를 입력해주세요==========");
-        System.out.println("============입력(q) 시스템 종료============");
-        Scanner memoIndexInput = new Scanner(System.in);
-        int enterIndex = memoIndexInput.nextLine();
-        if (enterIndex.equals(getMenoIndex[]))
-        MenuItem menuItem = menuService.getMenuItem(1, menuName);
-        System.out.println("입력받은 메뉴 이름 : " + menuName);
-    }
-
-    public enterPassword() {
-        System.out.println("============비밀번호를 입력해주세요=========");
-        System.out.println("============입력(q) 시스템 종료============");
-        Scanner memoPasswordInput = new Scanner(System.in);
-        int enterPassword = memoPasswordInput.nextLine();
-        System.out.println("입력받은 메뉴 이름 : " + menuName);
-        MenuItem menuItem = menuService.getMenuItem(1, menuName);
+//    public enterIndex() {
+//        System.out.println("============글 번호를 입력해주세요==========");
+//        System.out.println("============입력(q) 시스템 종료============");
+//        Scanner memoIndexInput = new Scanner(System.in);
+//        int enterIndex = memoIndexInput.nextLine();
+//        if (enterIndex.equals(getMenoIndex[]))
+//        MenuItem menuItem = menuService.getMenuItem(1, menuName);
+//        System.out.println("입력받은 메뉴 이름 : " + menuName);
+//    }
+//
+//    public enterPassword() {
+//        System.out.println("============비밀번호를 입력해주세요=========");
+//        System.out.println("============입력(q) 시스템 종료============");
+//        Scanner memoPasswordInput = new Scanner(System.in);
+//        int enterPassword = memoPasswordInput.nextLine();
+//        System.out.println("입력받은 메뉴 이름 : " + menuName);
+//        MenuItem menuItem = menuService.getMenuItem(1, menuName);
     }
 }
